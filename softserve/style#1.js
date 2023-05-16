0e28fa98edcff42c0df0676ddd0c4221e1b29d83
Marharyta Kysla <112627440+MargoKis@users.noreply.github.com>
2023-05-17 01:55:56 +0300

    Add files via upload

---
+++ softserve/style#1.js
@@ -0,0 +1,67 @@
+console.log('Kysla')
+
+////////////////////////
+
+let a = 'fine';
+let b = 'good';
+
+alert(a)
+alert(b)
+
+let c = a
+
+alert(a)
+alert(b)
+alert(c)
+
+////////////////////////
+
+let obj = {
+    name: 'Fill',
+    age: 21,
+    married: false,
+    hasPet: undefined,
+    student: null,
+}
+
+////////////////////////
+
+let isAdult = confirm('Are you an adult?');
+alert(isAdult)
+console.log(isAdult)
+
+////////////////////////
+
+let name = 'Marharyta';
+let surname = 'Kysla';
+let schoolNum = 37;
+let yearOfBirth = 2000;
+let married = false;
+
+alert(schoolNum);
+alert(married);
+alert(surname)
+
+let number;
+alert(typeof(number))
+
+let miss = null;
+alert(typeof(miss))
+
+////////////////////////
+
+let log = prompt('Your login')
+let mail = prompt('Your email')
+let pas = prompt('Your password')
+
+alert(`Your login is ${log}, your email is ${mail} and your password is ${pas}`)
+
+////////////////////////
+
+let hour = (60 * 60);
+let day = (60 * 60 * 24);
+let month = (60 * 60 * 24 * 30)
+
+alert(hour)
+alert(day)
+alert(month)
